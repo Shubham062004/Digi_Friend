02298c5ae2a46af8907dc66719cf951107ec0969
Shubham <shubhamkumar081908@gmail.com>
2025-01-04 22:06:18 +0530

    changes

--- Client/src/components/LandingPage.jsx
+++ Client/src/components/LandingPage.jsx
@@ -241,10 +241,7 @@ const LandingPage = () => {
                   </div>
                   <div className="flex justify-center">
                     <Link
-                      // to="/booking"
-                      onClick={handleScheduleMeeting}
-                      to="/booking"
-
+                      to="https://cal.com/shubham-kumar-chaurasia-fll1ki"
                       className="inline-block bg-blue-500 text-white py-2 px-6 rounded hover:bg-blue-600 transition duration-300 text-center w-full md:w-auto"
                     >
                       Book Now
